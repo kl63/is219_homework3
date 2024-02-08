ac3564ff7fbf8acecdd662d77c74d8dd07b31bac
kl63 <kl63@NJIT.edu>
2024-02-08 17:50:13 -0500

    part2 starting files

---
+++ calculator/__init__.py
@@ -0,0 +1,22 @@
+from calculator.calculation import Calculation
+from calculator.operations import add, subtract, multiply, divide
+
+class Calculator:
+    @staticmethod
+    def add(a,b):
+        calculation = Calculation(a, b, add)  # Pass the add function from calculator.operations
+        return calculation.get_result()
+    @staticmethod
+    def subtract(a,b):
+        calculation = Calculation(a, b, subtract)  # Pass the add function from calculator.operations
+        return calculation.get_result()
+    @staticmethod
+    def multiply (a,b):
+        calculation = Calculation(a, b, multiply)  # Pass the add function from calculator.operations
+        return calculation.get_result()
+    @staticmethod
+    def divide(a,b):
+        calculation = Calculation(a, b, divide)  # Pass the add function from calculator.operations
+        return calculation.get_result()
+
+
